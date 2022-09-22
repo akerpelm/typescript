ba4962c0b3d496a091b46fee52f312f7f560c8cd
akerpelm <akerpelm@u.rochester.edu>
2022-09-22 13:47:41 -0400

    continue sorter

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,20 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // All of this works only if collection is a number[]
+        // If collection is an array of numbers.
         if (this.collection[j] > this.collection[j + 1]) {
           const leftElement = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = leftElement;
         }
+
+        // Only works when collection is a string
+        // If collection is an string, do this logic instead.
       }
     }
   }
