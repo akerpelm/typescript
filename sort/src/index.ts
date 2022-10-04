e98a8b78c85f3a7703903e1ec42cc79bb73dfc12
akerpelm <akerpelm@u.rochester.edu>
2022-10-03 21:06:15 -0400

    begin sortable characters

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,25 +1,10 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // All of this works only if collection is a number[]
-        // If collection is an array of numbers.
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftElement = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftElement;
-        }
-
-        // Only works when collection is a string
-        // If collection is an string, do this logic instead.
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection('xZSzaYslkB');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
