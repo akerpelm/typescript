a45c94330704165acff284598c35ff5eb16ab5eb
akerpelm <akerpelm@u.rochester.edu>
2022-11-23 19:57:48 +0100

    begin express TS integration

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
