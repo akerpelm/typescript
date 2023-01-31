3cf33d8cbf87f620c26abe542810ecedad86ec06
akerpelm <akerpelm@u.rochester.edu>
2023-01-30 20:15:01 -0500

    begin controller decorator:

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -77,3 +77,15 @@ router.get('/protected', requireAuth, (req: Request, res: Response) => {
 });
 
 export { router };
+
+function post(routeName: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    router.post(routeName, target[key]);
+  };
+}
+
+function use(middleware: any) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    router;
+  };
+}
