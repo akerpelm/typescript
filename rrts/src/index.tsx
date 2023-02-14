320a0b6cd011792740363403dd70ee019fe75cb5
akerpelm <akerpelm@u.rochester.edu>
2023-02-13 22:23:25 -0500

    add redux

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
-
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>;
-// };
-
-class App extends Component<AppProps> {
-  state = {
-    counter: 0
-  };
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDom.render(<App color="red" />, document.querySelector('#root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
